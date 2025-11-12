d5399dc7f39e4a4aa6d3101f05202a4fcc6da126
Robert <robert11122002@gmail.com>
2025-11-12 22:05:47 +0000

    package for windows works. Splits scripts into separate files

---
+++ package-code.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+export PYI_MAKESPEC
+export PYINSTALLER
+
+set -e
+
+function _source_venv() {
+    # Since bash CAN be run on windows, and we will use it in CI/CD pipelines.
+    # Windows, for whatever reason, puts it in `Scripts` instead of `bin`.
+    local NORMAL_VENV=".venv/bin/activate"
+    local WINDOWS_VENV=".venv/Scripts/activate"
+    if test -f $NORMAL_VENV; then
+        echo "Using normal venv"
+        source ./$NORMAL_VENV
+    elif test -f $WINDOWS_VENV; then
+        echo "Using windows venv"
+        source ./$WINDOWS_VENV
+    fi
+}
+
+function _ensure_venv() {
+    if [ ! -d ".venv" ]; then
+        python3 -m venv .venv
+    fi
+}
+
+function main() {
+    echo "Building standalone executable using PyInstaller."
+
+    echo "Creating spec file..."
+    $PYI_MAKESPEC gui/main.py --name emulator
+
+    echo "Building executable..."
+    # use pyinstaller to create a standalone binary.
+    # --onefile: create a single executable file, no additional directories needed.
+    # --name emulator: name the output executable "emulator"
+    # --additional-hooks-dir hooks: include additional hooks from the "hooks" directory.
+    # --add-data "assets:assets": include the assets directory in the executable.
+    export PYTHONOPTIMIZE=1
+    $PYINSTALLER --onefile --name emulator --additional-hooks-dir hooks --add-data "assets:assets" -y gui/main.py
+}
+
+main
---
+++ package-windows.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export PYI_MAKESPEC=pyi-makespec.exe
+export PYINSTALLER=pyinstaller.exe
+
+bash package-code.sh
