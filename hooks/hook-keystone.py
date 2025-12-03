5a4e0d03f28b0593cf822956fbd4bcab09aef774
Robert Williamson <robert11122002@gmail.com>
2025-12-03 13:17:24 +0000

    run ruff format

--- hooks/hook-keystone.py
+++ hooks/hook-keystone.py
@@ -2,4 +2,4 @@
 from PyInstaller.utils.hooks import collect_all
 
 # Collect everything: compiled DLLs, python files, and data files
-datas, binaries, hiddenimports = collect_all('keystone')
+datas, binaries, hiddenimports = collect_all("keystone")
