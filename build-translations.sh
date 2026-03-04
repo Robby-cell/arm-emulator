7d339956ed996bf9cdafa7e2b23c32ace1a3ebd5
Robert Williamson <robert11122002@gmail.com>
2026-03-04 18:43:31 +0000

    removes the old translations when building translations.

--- build-translations.sh
+++ build-translations.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-uv run scripts/build_translations.py --languages \
+BASE=$(dirname "$0")
+
+function _remove_old_translations() {
+    echo "Removing old translations..."
+
+    base_dir="${BASE}/assets/translations"
+    for file in $(ls ${base_dir} | grep -E 'app_(\w{2})\.qm$'); do
+        translation_path="${base_dir}/${file}"
+        echo "Removing ${translation_path}"
+        rm "${translation_path}"
+    done
+
+    echo "Old translations removed."
+}
+
+_remove_old_translations
+
+uv run "${BASE}/scripts/build_translations.py" --dir="${BASE}/gui" --translations_dir="${BASE}/assets/translations" \
+    --languages \
     en ru pl es ar
